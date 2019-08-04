41f5c18f41b903c80d766280eaed7cad71719d23
systena <systena@PCK-137-296>
2019-08-04 21:06:34 +0900

    20190805

--- src/main/java/jp/co/systena/tigerscave/ListController_1/ListController.java
+++ src/main/java/jp/co/systena/tigerscave/ListController_1/ListController.java
@@ -1,72 +1,98 @@
 package jp.co.systena.tigerscave.ListController_1;
 
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 public class ListController {
+
+@Autowired
+HttpSession session;
+
+//  商品一覧画面表示
 @RequestMapping("/show")
-//@GetMapping("/show")
 public ModelAndView show(ModelAndView mav) {
   mav.addObject("Item", new Item());
-  //model.addAttribute("Item", new Item());
   mav.setViewName("show");
   return mav;
 }
 
-//@RequestMapping(path = "/order", method = RequestMethod.POST)
-//public String submit(@ModelAttribute("param1") String param1, @ModelAttribute("param2") String param2) {
-
-//    return "order";
-//}
-
-//@RequestMapping(path = "/order", method = RequestMethod.GET)
-//@GetMapping
-//@PostMapping
-//public ModelAndView init(ModelAndView mav2) {
-//  mav2.addObject("ListForm",new ListForm());
-//  mav2.setViewName("order");
-//  return mav2;
-//}
-
-//@RequestMapping(path = "/order", method = RequestMethod.POST)
-//@PostMapping
-//@GetMapping
-//public String submit(ListForm form) {
-//  System.out.println("form.value=" + form.getNum1());
-//  return "order";
-//}
-
-//  入力画面
-@GetMapping("/input")
+//  注文入力画面表示(カート追加)
+@GetMapping("/order")
 public String input(@ModelAttribute("listform") ListForm listform) {
-    return "input";
+
+  //値が設定されているかコンソールに表示してみる(debug)
+  //System.out.println("a");
+  //System.out.printf("%d\n",listform.getNum1());
+
+  return "order";
 }
 
-//  書籍情報画面
-@PostMapping("/output")
-public String toBookInfo(@ModelAttribute("listform") ListForm listform) {
-    return "output";
+//  注文合計画面表示(カート表示)
+@GetMapping("/output")
+public String output(@ModelAttribute("listform") ListForm listform) {
+
+  int session_num1;
+  int session_num2;
+
+  //値が設定されているかコンソールに表示してみる(debug)
+  //System.out.println("b");
+  //System.out.printf("%d\n",listform.getNum1());
+  //System.out.println((String)session.getAttribute("num1"));
+
+  //セッションに保存している注文済数量を取り出して新たに注文された数量をプラスしてセッションに保存。
+  if(((String)session.getAttribute("num1")) == null) {
+    session_num1 = 0;
+  }else {
+    session_num1 = Integer.parseInt((String)session.getAttribute("num1"));
+  }
+  session.setAttribute("num1", Integer.toString(session_num1 + listform.getNum1()));
+
+  //値が設定されているかコンソールに表示してみる(debug)
+  //System.out.println("c");
+  //System.out.println((String)session.getAttribute("num1"));
+  //System.out.printf("%d\n",Integer.parseInt((String)session.getAttribute("num1")));
+
+  //注文合計数をlistformに更新
+  listform.setNum1(Integer.parseInt((String)session.getAttribute("num1")));
+
+  //注文合計金額をlistformに更新
+  listform.setNum1_sum(new Item().price1);
+
+  //以下商品Item2でも同様に実施
+  if(((String)session.getAttribute("num2")) == null) {
+    session_num2 = 0;
+  }else {
+    session_num2 = Integer.parseInt((String)session.getAttribute("num2"));
+  }
+  session.setAttribute("num2", Integer.toString(session_num2 + listform.getNum2()));
+
+  //注文合計数をlistformに更新
+  listform.setNum2(Integer.parseInt((String)session.getAttribute("num2")));
+
+  //注文合計金額をlistformに更新
+  listform.setNum2_sum(new Item().price2);
+
+  return "output";
 }
 
-
+//商品一覧は固定で設定
 public static class Item {
 
   public int itemId1 = 1;
-  public String name1 = "name1";
+  public String name1 = "商品1";
   public int price1 = 1000;
 
   public int itemId2 = 2;
-  public String name2 = "name2";
+  public String name2 = "商品2";
   public int price2 = 2000;
 
-
-
 }
-
 
 }
--- src/main/java/jp/co/systena/tigerscave/ListController_1/ListForm.java
+++ src/main/java/jp/co/systena/tigerscave/ListController_1/ListForm.java
@@ -2,24 +2,56 @@ package jp.co.systena.tigerscave.ListController_1;
 
 public class ListForm {
 
-  private String itemId1;
-  private String itemId2;
-//  private int num1;
-//  private int num2;
-  private int num1;
-  private String num2;
+  private int num1;         //商品番号1 注文数量
+  private int num1_sum;     //商品番号1 注文合計金額
+  private int num2;         //商品番号2 数量
+  private int num2_sum;     //商品番号2 注文合計金額
 
   public int getNum1() {
     return this.num1;
   }
+
   public void setNum1(int num1) {
+
+    //値が設定されているかコンソールに表示してみる(debug)
+    //System.out.println("d");
+    //System.out.printf("%d\n",this.num1);
+
     this.num1 = num1;
+
+    //値が設定されているかコンソールに表示してみる(debug)
+    //System.out.printf("%d\n",this.num1);
   }
 
-  public String getNum2() {
+  public int getNum1_sum() {
+    return this.num1_sum;
+  }
+
+  public void setNum1_sum(int num1_sum) {
+
+    //値が設定されているかコンソールに表示してみる(debug)
+    //System.out.println("e");
+    //System.out.printf("%d\n",this.num1_sum);
+
+    this.num1_sum = num1_sum * this.num1;       //注文合計数*単価=注文合計金額
+
+    //System.out.printf("%d\n",this.num1_sum);
+  }
+
+  public int getNum2() {
     return this.num2;
   }
-  public void setNum2(String num2) {
-    this.num2 += num2;
+
+  public void setNum2(int num2) {
+    this.num2 = num2;
   }
+
+  public int getNum2_sum() {
+    return this.num2_sum;
+  }
+
+  public void setNum2_sum(int num2_sum) {
+    this.num2_sum = num2_sum * this.num2;       //注文合計数*単価=注文合計金額
+  }
+
 }
