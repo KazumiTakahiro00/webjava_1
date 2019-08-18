2a9a4011f3887053e89d24bb2afbec28e187baea
systena <systena@PCK-137-296>
2019-08-18 19:56:33 +0900

    20190818

--- src/main/java/jp/co/systena/tigerscave/ListController_1/ListController.java
+++ src/main/java/jp/co/systena/tigerscave/ListController_1/ListController.java
@@ -1,13 +1,14 @@
 package jp.co.systena.tigerscave.ListController_1;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 
 @Controller
 public class ListController {
@@ -23,28 +24,29 @@ public ModelAndView show(ModelAndView mav) {
   return mav;
 }
 
-//  注文入力画面表示(カート追加)
-@GetMapping("/order")
-public String input(@ModelAttribute("listform") ListForm listform) {
-
-  //値が設定されているかコンソールに表示してみる(debug)
-  //System.out.println("a");
-  //System.out.printf("%d\n",listform.getNum1());
-
-  return "order";
-}
-
 //  注文合計画面表示(カート表示)
-@GetMapping("/output")
-public String output(@ModelAttribute("listform") ListForm listform) {
+@GetMapping("/order")
+public ModelAndView order(ModelAndView mav, @ModelAttribute("listform") ListForm listform,BindingResult bindingResult, HttpServletRequest request) {
 
   int session_num1;
   int session_num2;
 
   //値が設定されているかコンソールに表示してみる(debug)
   //System.out.println("b");
+  //System.out.println(listform);
   //System.out.printf("%d\n",listform.getNum1());
   //System.out.println((String)session.getAttribute("num1"));
+
+  //エラーがある場合はそのまま戻す
+  if (bindingResult.getAllErrors().size() > 0) {
+    //値が設定されているかコンソールに表示してみる(debug)
+    //System.out.println("a");
+
+    mav.addObject("listform", listform);
+    mav.setViewName("order");
+
+    return mav;
+  }
 
   //セッションに保存している注文済数量を取り出して新たに注文された数量をプラスしてセッションに保存。
   if(((String)session.getAttribute("num1")) == null) {
@@ -63,7 +65,7 @@ public String output(@ModelAttribute("listform") ListForm listform) {
   listform.setNum1(Integer.parseInt((String)session.getAttribute("num1")));
 
   //注文合計金額をlistformに更新
-  listform.setNum1_sum(new Item().price1);
+  listform.setNum1_sum(Item.price1);
 
   //以下商品Item2でも同様に実施
   if(((String)session.getAttribute("num2")) == null) {
@@ -77,9 +79,12 @@ public String output(@ModelAttribute("listform") ListForm listform) {
   listform.setNum2(Integer.parseInt((String)session.getAttribute("num2")));
 
   //注文合計金額をlistformに更新
-  listform.setNum2_sum(new Item().price2);
+  listform.setNum2_sum(Item.price2);
 
-  return "output";
+  mav.addObject("listform", listform);
+  mav.setViewName("order");
+  return mav;
+
 }
 
 //商品一覧は固定で設定
@@ -87,11 +92,11 @@ public static class Item {
 
   public int itemId1 = 1;
   public String name1 = "商品1";
-  public int price1 = 1000;
+  public static int price1 = 1000;
 
   public int itemId2 = 2;
   public String name2 = "商品2";
-  public int price2 = 2000;
+  public static int price2 = 2000;
 
 }
 
